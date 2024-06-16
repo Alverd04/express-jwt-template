816692507ac10b722e00882b04bed5229af6acac
Alverd04 <albert.font.fernandez@gmail.com>
2024-06-16 14:20:41 +0200

    remove mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 import app from "./app";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 5000;
 
-const DATABASE_URL =
-  process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development"
-    ? process.env.MONGODB_DEV_URI
-    : process.env.MONGODB_URI;
+// const DATABASE_URL =
+//   process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development"
+//     ? process.env.MONGODB_DEV_URI
+//     : process.env.MONGODB_URI;
 
 const start = async () => {
   try {
-    if (DATABASE_URL) {
-      await mongoose.connect(DATABASE_URL || "", {
-        dbName: "rev-hub",
-      });
-    }
+    // await mongoose.connect(DATABASE_URL || "", {
+    //   dbName: "rev-hub",
+    // });
+
     app.listen(PORT, () =>
       console.log(`Server is running on  http://localhost:${PORT}`)
     );
